ff30fa88ff4cd17453db15052e3b1bdaf746f35d
noureen <noureenamer0@gmail.com>
2023-06-16 19:37:09 -0700

    m

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,19 +6,19 @@
  * Description: print 0-9 using putchar
  *              while using int variable
  *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Always  0(success)
+ */
 int main(void)
 {
 int digit = 0;
 
 while (digit <= 9)
 {
+/*convert digits to ASCII representation*/
 putchar(digit + '0');
 digit++;
 }
-putcha('\n');
+putchar('\n');
 
 return (0);
-}
+}}
