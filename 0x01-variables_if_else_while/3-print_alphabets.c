6b7f985ae355d6dc7ab94724d91fd8bdba2f4cc8
noureen <noureenamer0@gmail.com>
2023-06-16 17:18:28 -0700

    xx

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,27 +3,27 @@
 /**
  * main - Entry point
  *
- * Description: print all alphabet letters
+ * Description: print alphabet in lowercase then in uppercase
  *
- * Return: 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 char ch = 'a', CH = 'A';
 
-/*prints a -z*/
 while (ch <= 'z')
 {
 putchar(ch);
-ch++; 
+ch++;
 }
-/*prints A - Z*/
-while (ch <= 'Z')
+
+while (CH <= 'Z')
 {
 putchar(CH);
 CH++;
 }
 putchar('\n');
+
 return (0);
 }
