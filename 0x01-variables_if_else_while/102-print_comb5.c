236d53f3861b52940c461d899ee0205fcafaf58d
noureen <noureenamer0@gmail.com>
2023-06-19 11:36:35 -0700

    xx

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program prints all possible diffrent combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int firstdigit = 0, secondDigit;
+while (firstdigit <= 99)
+{
+secondDigit = firstdigit;
+while (secondDigit <= 99)
+{
+if (secondDigit != firstdigit)
+{
+putchar((firstdigit / 10) + 48);
+putchar((firstdigit % 10) + 48);
+putchar(' ');
+putchar((secondDigit / 10) + 48);
+putchar((secondDigit % 10) + 48);
+if (firstdigit != 99 || secondDigit != 99)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+secondDigit++;
+}
+firstdigit++;
+}
+putchar('\n');
+return (0);
+}
