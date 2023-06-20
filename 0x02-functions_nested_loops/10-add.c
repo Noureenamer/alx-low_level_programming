f9300a92208ee765e9fef30605a6340cce97a937
noureen <noureenamer0@gmail.com>
2023-06-20 16:58:47 -0700

    xx

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Function to return 1 or -1 if printing a character.
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
