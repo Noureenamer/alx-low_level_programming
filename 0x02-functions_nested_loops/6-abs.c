a5d9fde9b444bdc850ed7264fdca65c66c4b5d2f
noureen <noureenamer0@gmail.com>
2023-06-20 16:01:20 -0700

    xx

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Function to return 1 or -1 if printing a character.
+ *
+ * @n: charachter to return.
+ *
+ * Return: Always 0 (Success)
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (0);
+}
+
