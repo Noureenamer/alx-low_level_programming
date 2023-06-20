edb97739b717a25bf304e5ce3432959a99ce293b
noureen <noureenamer0@gmail.com>
2023-06-20 16:07:47 -0700

    xx

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _abs - Function to return 1 or -1 if printing a character.
+ * _abs - Function that computes the absolute value of integer
  *
- * @n: charachter to return.
+ * @n: takes in integer type input for function
  *
  * Return: Always 0 (Success)
 */
+
 int _abs(int n)
 {
 	if (n < 0)
 		n = (-1) * n;
-	return (0);
+	return (n);
 }
 
