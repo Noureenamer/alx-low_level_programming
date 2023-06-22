2e524446b0c8995a97586a0189f968895e6b8a49
noureen <noureenamer0@gmail.com>
2023-06-22 06:59:55 -0700

    xx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function to calculate multiply of two integer
+ *
+ * @a: first input of function
+ * @b: second input of function
+ * Return: return 1 if 'c' is lowercase
+ *         otherwise always 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
