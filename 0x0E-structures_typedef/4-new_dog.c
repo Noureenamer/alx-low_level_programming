449015e4896a9cf34e40c7f5dd7e093b6ceb301c
noureen <noureenamer0@gmail.com>
2023-07-17 18:06:24 -0700

    xx

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,7 +20,6 @@ int _strlen(char *s)
 
 	return (i);
 }
-
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
@@ -49,7 +48,6 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
-
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
